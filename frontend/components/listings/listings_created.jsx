2063d058580c53aa8060339a53cf821159d7407b
Elhoussine Elouardy <elouardy.elhoussine@gmail.com>
2022-05-03 00:35:16 -0700

    new listing form styling

--- frontend/components/listings/listings_created.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default () => {
-  return (
-    <div id="listings-success">
-      Success! New listing created.
-    </div>
-  );
-}
--- frontend/components/listings/listings_form.jsx
+++ frontend/components/listings/listings_form.jsx
@@ -75,7 +75,7 @@ export default class ListingsForm extends React.Component {
   renderErrors() {
     return (
       <ul className="error-list">
-        {this.state.errors.map((error, i) => (
+        {this.errors.map((error, i) => (
           <li className="error-item" key={`error-${i}`}>
             {error}
           </li>
@@ -104,8 +104,79 @@ export default class ListingsForm extends React.Component {
           <br />
           <span> </span>
           <br />
+          <div className='listing-form-col'>
+            <div className='listing-form-cel'>
+              <label htmlFor="category">Category</label>
+              <select name="product[category_id]" value={this.state.category_id} onChange={this.updateSelect('category_id')}>
+                <option value="1">Sneakers</option>
+                <option value="2">Apparel</option>
+                <option value="3">Electronics</option>
+                <option value="4">Trading Cards</option>
+                <option value="5">Collectibles</option>
+                <option value="6">Accessories</option>
+                <option value="7">NFTs</option>
+              </select>
+            </div>
+            <div className='listing-form-cel'>
+              <label htmlFor="">Name</label>
+              <input type="text" name="product[name]" id="" value={this.state.name} onChange={this.update('name')} />
+            </div>
+          </div>
+          <div className='listing-form-col'>
+            <div className='listing-form-cel'>
+              <label htmlFor="">Style</label>
+              <input type="text" name="product[style]" id="" value={this.state.style} onChange={this.update('style')} />
+            </div>
+            <div className='listing-form-cel'>
+              <label htmlFor="">Color</label>
+              <input type="text" name="product[color]" id="" value={this.state.color} onChange={this.update('color')} />
+            </div>
+          </div>
+          <div className='listing-form-col'>
+            <div className='listing-form-cel'>
+              <label htmlFor="">Brand</label>
+              <input type="text" name="product[brand]" id="" value={this.state.brand} onChange={this.update('brand')} />
+            </div>
+            <div className='listing-form-cel'>
+              <label htmlFor="">Price $$</label>
+              <input type="number" name="product[price]" id="" value={this.state.price} onChange={this.update('price')} />
+            </div>
+          </div>
+          <div className='listing-form-col'>
+            <div className='listing-form-cel'>
+              <label htmlFor="">Release Date</label>
+              <input type="date" name="product[release_date]" id="" value={this.state.release_date} onChange={this.update('release_date')} />
+            </div>
+            <div className='listing-form-cel'>
+              <label htmlFor="">Description</label>
+              <textarea name="product[description]" id="" value={this.state.description} onChange={this.update('description')} />
+              
+            </div>
+          </div>
+          <div className='listing-form-col'>
+            <div className='listing-form-cel'>
+              <label htmlFor="">Image</label>
+              <input type="file" name="product[image]" id="" onChange={this.handleFile} />
+            </div>
+            <div className='listing-form-cel'>
+            </div>
+          </div>
+
           <br />
-          <label htmlFor="category">Category
+          <span> </span>
+          <br />
+          <div className='listing-form-col'>
+            <div className='listing-form-cel'>
+              <div id="listings-form-errors" className="hidden">
+                {this.renderErrors()}
+              </div>
+            </div>
+            <div className='listing-form-cel'>
+              <input type="submit" value="Add Product" id="listings-form-submit" />
+            </div>
+          </div>
+          <br />
+          {/* <label htmlFor="category">Category
             <select name="product[category_id]" value={this.state.category_id} onChange={this.updateSelect('category_id')}>
               <option value="1">Sneakers</option>
               <option value="2">Apparel</option>
@@ -116,27 +187,6 @@ export default class ListingsForm extends React.Component {
               <option value="7">NFTs</option>
             </select>
           </label>
-          <br />
-          <label htmlFor="">Name
-            <input type="text" name="product[name]" id="" value={this.state.name} onChange={this.update('name')}/>
-          </label>
-          <br />
-          <label htmlFor="">Style
-            <input type="text" name="product[style]" id="" value={this.state.style} onChange={this.update('style')}/>
-          </label>
-          <br />
-          <label htmlFor="">Color
-            <input type="text" name="product[color]" id="" value={this.state.color} onChange={this.update('color')}/>
-          </label>
-          <br />
-          <label htmlFor="">Brand
-            <input type="text" name="product[brand]" id="" value={this.state.brand} onChange={this.update('brand')}/>
-          </label>
-          <br />
-          <label htmlFor="">Price $$
-            <input type="number" name="product[price]" id="" value={this.state.price} onChange={this.update('price')} />
-          </label>
-          <br />
           <label htmlFor="">Image
             <input type="file" name="product[image]" id="" onChange={this.handleFile}/>
           </label>
@@ -152,7 +202,7 @@ export default class ListingsForm extends React.Component {
           <br />
           <span> </span>
           <br />
-          <input type="submit" value="Add product" id="listings-form-submit" />
+          <input type="submit" value="Add product" id="listings-form-submit" /> */}
         </form>
         <div id="listings-form-errors" className="hidden">
           {/* {this.renderErrors()} */}
